32ecbc38c496e5a0216d94fbd3fedfde874a9b60
JulFarn <julfarn@gmx.de>
2023-12-17 21:48:53 +0100

    improve day 16

--- 2023/Day 16/part1.py
+++ 2023/Day 16/part1.py
@@ -1,13 +1,40 @@
 
-input_strings = open("input_ex.txt").read().splitlines()
+input_strings = open("F:\\Projekte\\AdventOfCode\\2023\\Day 15\\input_ex.txt").read().splitlines()
 
-def hash(s):
-    cuva = 0
-    for c in s:
-        cuva += ord(c)
-        cuva *= 17
-        cuva %= 256
-    return cuva
+x_len = len(input_strings[0])
+y_len = len(input_strings)
 
-hashs = [hash(s) for s in input_strings]
-print(sum(hashs))
+energized_map = [[False for x in range(x_len)] for y in range(y_len)]
+
+beams = [{'x':-1, 'y':0, 'dir': "e"}]
+
+def reflect(beam,mirror):
+    if beam['dir'] == 'n':
+        if mirror == '/': beam['dir'] = 'e'
+        elif mirror == '\\': beam['dir'] = 'w'
+    elif beam['dir'] == 'e':
+        if mirror == '/': beam['dir'] = 'n'
+        elif mirror == '\\': beam['dir'] = 's'
+    elif beam['dir'] == 's':
+        if mirror == '/': beam['dir'] = 'w'
+        elif mirror == '\\': beam['dir'] = 'e'
+    elif beam['dir'] == 'w':
+        if mirror == '/': beam['dir'] = 's'
+        elif mirror == '\\': beam['dir'] = 'n'
+
+def propagate(beam):
+    if beam['dir'] == 'n': beam["y"]-=1
+    if beam['dir'] == 's': beam["y"]+=1
+    if beam['dir'] == 'e': beam["x"]+=1
+    if beam['dir'] == 'w': beam["x"]-=1
+
+def split(beam):
+    if beam['dir'] == 'n' or beam['dir'] == 's':
+        beam['dir'] = 'e'
+        return {'x': beam['x'], 'y': beam['y'], 'dir': 'w'}
+    elif beam['dir'] == 'e' or beam['dir'] == 'w':
+        beam['dir'] = 'n'
+        return {'x': beam['x'], 'y': beam['y'], 'dir': 's'}
+
+while(len(beams)>0):
+    # kill a beam if it hits an energized splitter.
